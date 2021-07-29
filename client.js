698914d8d6c23edf83adebbc688d5d30a5dd00e5
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-07-29 00:17:35 +0000

    Added setupInput connection that allows our client to register input from the keyboard

--- client.js
+++ client.js
@@ -1,14 +1,12 @@
 const { builtinModules } = require("module");
 const net = require("net");
 
-// establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
     host: '135.23.223.133',
     port: 50542
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on('connect', () => {
